d9b4f766d7f0dc39082191013c8f37e913f4adb6
jjteoh-pingspace <teoh.jia.jun@pingspace.co>
2024-08-13 14:56:38 +0800

    feat: read opensearch connection from appsettings

--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ using OpenSearch.Net;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+var configuration = builder.Configuration;
+
 // Configure OpenSearch client
 builder.Services.AddSingleton<IOpenSearchClient>(sp =>
 {
-    var settings = new ConnectionSettings(new Uri("https://18.141.197.9:9200"))
-    .BasicAuthentication("admin", "aRT/[sqA7^")
+    // read credentials from appsettings
+    var openSearchUrl = configuration.GetValue<string?>("OpenSearch:HostUrl") ?? "https://18.141.197.9:9200";
+    var username = configuration.GetValue<string?>("OpenSearch:UserName") ?? "admin";
+    var password = configuration.GetValue<string?>("OpenSearch:Password") ?? "Abc123";
+
+    var settings = new ConnectionSettings(new Uri(openSearchUrl))
+    .BasicAuthentication(username, password)
     .ServerCertificateValidationCallback(CertificateValidations.AllowAll);
     return new OpenSearchClient(settings);
 });
